08a9d44b1835ed08e291e29f0e342f29227a5c6a
Vítor Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2018-05-09 15:31:14 -0300

    Version 0.6.0b15 (Beta 15)  - Fixed import recursion again

--- aioreactive/disposable/__init__.py
+++ aioreactive/disposable/__init__.py
@@ -1,7 +1,9 @@
-from .anonymous_disposable import AnonymousDisposable
-from .composite_disposable import CompositeDisposable
 from ..abstract import Disposable
 
 
 async def adispose(disposable: Disposable):
     await disposable.__adispose__()
+
+
+from .anonymous_disposable import AnonymousDisposable
+from .composite_disposable import CompositeDisposable
