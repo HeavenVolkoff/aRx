0b1870ed4b7af3bf9efd89a8f1bfc7e98fee226c
Vítor Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2018-05-09 17:01:37 -0300

    Version 0.6.016 (Beta 16)  - Fixed filter __asend__ recursion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='aioreactive',
-    version='0.6.0b15',
+    version='0.6.0b16',
     description='Async/await Reactive Tools for Python 3.6+',
     long_description=(
         "aioreactive is a library for asynchronous and reactive "
