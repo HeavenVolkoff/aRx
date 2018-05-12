23d7d743ef8f2a30ba4d19f04e5b86bb459863ef
Vítor Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2018-05-11 21:35:40 -0300

    Version 0.6.0b22 (Beta 22)  - Attempt fix MultiStream concurrency on observer close

--- aioreactive/observer/base.py
+++ aioreactive/observer/base.py
@@ -45,6 +45,11 @@ class BaseObserver(Disposable, Loggable, Observer[K], metaclass=ABCMeta):
             self, self.aclose, loop=self.loop, logger=self.logger
         )
 
+    def __del__(self):
+        # This can possibly fuck the garbage collector up
+        # If you experience any weird bug delete this
+        self.loop.create_task(self.aclose())
+
     async def __adispose__(self):
         """Implements the disposable interface, enables context management"""
         await self.aclose()
