a0859189780c68ab2e48d8c8a636dd4c5e334292
Vítor Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2018-05-18 20:30:30 -0300

    Version 0.6.0rc4 (Release Canditate 4)  - Added loggable parameter to change logger level

--- areact/abstract/loggable.py
+++ areact/abstract/loggable.py
@@ -5,7 +5,13 @@ from logging import Logger, getLogger
 
 
 class Loggable(object):
-    def __init__(self, *, parent_logger: T.Optional[Logger] = None, **kwargs):
+    def __init__(
+        self,
+        *,
+        logger_level: T.Optional[T.Union[str, int]] = None,
+        parent_logger: T.Optional[Logger] = None,
+        **kwargs
+    ):
         cls = type(self)
 
         super().__init__(**kwargs)
@@ -14,3 +20,6 @@ class Loggable(object):
             parent_logger.getChild(cls.__name__)
             if parent_logger else getLogger(cls.__name__)
         )
+
+        if logger_level is not None:
+            self.logger.setLevel(logger_level)
