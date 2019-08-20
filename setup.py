1ae7f3d55223664273d5c202e9e4e8cd9746797c
HeavenVolkoff <vasconcellos.dev@gmail.com>
2019-08-20 15:16:09 -0300

    Update foundation

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
 
-# Internal
+"""Entrypoint for setuptools package installation.
+    isort:skip_file
+"""
+
 import sys
-import shlex  # python>=3.3
-from os import path, scandir
 
-SETUP_CONFIG = "setup.cfg"
-VERSION_FILE = "VERSION"
-SOURCE_LOCATION = "src"
+if sys.version_info <= (3, 3):
+    raise RuntimeError("Only Python >= 3.4 supported")
 
-
-def main():
-    """Exec setup"""
-    from setuptools import setup, find_namespace_packages
-    from distutils.errors import DistutilsOptionError
-
-    try:
-        setup(packages=find_namespace_packages("src"), package_dir={"": "src"})
-    except DistutilsOptionError:
-        raise RuntimeError(
-            "Maybe the setuptools package is too old. To update it run:\n"
-            "{} -m pip install -U setuptools",
-            sys.executable,
-        )
-
+from os import path, chdir, scandir
+from shlex import quote
+from distutils.errors import DistutilsOptionError
 
 try:
     import pkg_resources
+    from setuptools import setup, find_namespace_packages
+    from setuptools.config import read_configuration
 except ImportError:
     raise RuntimeError(
         "The setuptools package is missing or broken. To (re)install it run:\n"
@@ -43,31 +33,36 @@ def has_requirement(req):
         return True
 
 
-if path.isfile(SETUP_CONFIG):
-    # Read setup configuration
-    from setuptools.config import read_configuration
+if __name__ == "__main__":
+    # Allow setup.py to run from another directory
+    chdir(path.dirname(__file__))
+
+    SETUP_CONFIG = "setup.cfg"
+
+    if not path.isfile(SETUP_CONFIG):
+        raise RuntimeError("Unsupported package structure, a setup.cfg file is required")
 
     config = read_configuration(SETUP_CONFIG)
     options = config.get("options", {})
     metadata = config.get("metadata", {})
-    setup_requires = tuple(
+    missing_install_requires = tuple(
         filter(lambda req: not has_requirement(req), options.get("setup_requires", []))
     )
 
-    if setup_requires:
+    if missing_install_requires:
         raise RuntimeError(
-            "Missing dependencies for installing {}. To proceed run:\n{} -m pip install {}".format(
+            "Missing dependencies to install {}. To fix run:\n{} -m pip install {}".format(
                 metadata.get("name", "this package"),
                 sys.executable,
-                " ".join(map(shlex.quote, setup_requires)),
+                " ".join(map(quote, missing_install_requires)),
             )
         )
 
-    version = metadata.get("version", None)
-    if version:
-        with scandir(SOURCE_LOCATION) as src:
-            for module in src:
-                if module.is_dir() and path.isfile(path.join(module.path, "__init__.py")):
-                    with open(path.join(module.path, VERSION_FILE), mode="w") as file:
-                        file.write(version)
-main()
+    try:
+        setup(packages=find_namespace_packages("src"), package_dir={"": "src"})
+    except DistutilsOptionError:
+        raise RuntimeError(
+            "Perhaps your setuptools package is too old. To update it run:\n"
+            "{} -m pip install -U setuptools",
+            sys.executable,
+        )
--- src/aRx/__init__.py
+++ src/aRx/__init__.py
@@ -1,11 +1,13 @@
-"""Asynchronous Reactive eXtensions."""
-
-__all__ = ("__version__",)
-
 # External
-import pkg_resources
+from importlib_metadata import version  # type: ignore
 
 try:
-    __version__ = str(pkg_resources.resource_string(__name__, "VERSION"), encoding="utf8")
-except (pkg_resources.ResolutionError, FileNotFoundError):
+    __version__: str = version(__name__)
+except Exception:  # pragma: no cover
+    import traceback
+    from warnings import warn
+
+    warn(f"Failed to set version due to:\n{traceback.format_exc()}", ImportWarning)
     __version__ = "0.0a0"
+
+__all__ = ("__version__",)
