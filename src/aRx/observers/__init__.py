fd5f0c2d12367c1f43b4c720bf6b0655824d998e
HeavenVolkoff <vasconcellos.dev@gmail.com>
2019-08-22 21:12:39 -0300

    Fix another export

--- src/aRx/observers/__init__.py
+++ src/aRx/observers/__init__.py
@@ -1,11 +1,11 @@
 """aRx observers implementations."""
 
-__all__ = ("consume", "Observer", "Consumer", "AnonymousObserver", "IteratorObserver")
+__all__ = ("Observer", "Consumer", "AnonymousObserver", "IteratorObserver")
 
 # External
 from aRx.observers.observer import Observer
 
 # Project
-from .consumer import Consumer, consume
+from .consumer import Consumer
 from .iterator_observer import IteratorObserver
 from .anonymous_observer import AnonymousObserver
